cac66cf4f0d175c21380c0cf41b22d3287e65be9
BaptBeuns <benetb@eleves.enpc.fr>
2016-02-02 15:31:51 +0100

    Pimp submit

--- source/submit.sh
+++ source/submit.sh
@@ -1,2 +1,7 @@
-tar -zcvf submit.tar.gz CMakeLists.txt build.sh run.sh main.cpp matrix.cpp matrix.h timing.h
+git checkout sequential
+tar -zcvf sequential.tar.gz CMakeLists.txt build.sh run.sh main.cpp matrix.cpp matrix.h timing.h
+
+git checkout omp
+tar -zcvf omp.tar.gz CMakeLists.txt build.sh run.sh main.cpp matrix.cpp matrix.h timing.h
+
 firefox http://codelab.necst.it/exs/BML
