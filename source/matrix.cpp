1bca7793f309bb4b4cf0558582953ebded7a95f8
BaptBeuns <benetb@eleves.enpc.fr>
2016-02-10 17:42:48 +0100

    Hotfix
    Last car doesn't move if first box not available at first

--- source/matrix.cpp
+++ source/matrix.cpp
@@ -71,11 +71,17 @@ bool Matrix::moveBlue() {
 
     for (int j=0; j<col; ++j){
 
+        // This boolean checks that first box is free at the beginning of the loop
+        bool first_isInitiallyFree = (this->operator()(0, j) == 0);
         int i = 0;
         while(i < row) {
             int *currentCar = &(this->operator()(i, j));
             int *nextCar = &(this->operator()((i+1)%row, j));
             bool next_isFree = (*nextCar == 0);
+            // Sometimes, when we arrive at the last box, we don't make the car move because the first box was originally not free
+            if (i == row - 1 and !first_isInitiallyFree) {
+                next_isFree = false;
+            }
 
             if(*currentCar == BLUE) {
                 if(next_isFree) {
@@ -103,11 +109,18 @@ bool Matrix::moveRed(){
 
     for (int i=0; i<row; ++i){
 
+        // This boolean checks that first box is free at the beginning of the loop
+        bool first_isInitiallyFree = (this->operator()(i, 0) == 0);
+
         int j = 0;
         while(j < col) {
             int *currentCar = &(this->operator()(i, j));
             int *nextCar = &(this->operator()(i, (j+1)%col));
             bool next_isFree = (*nextCar == 0);
+            // Sometimes, when we arrive at the last box, we don't make the car move because the first box was originally not free
+            if (j == col - 1 and !first_isInitiallyFree) {
+                next_isFree = false;
+            }
 
             if(*currentCar == RED) {
                 if(next_isFree) {
